2223bde08767bf673f907bfafcb2608efc3b4ea2
n1kford <n1kf0rd@mail.ru>
2021-09-25 11:40:02 +0600

    version 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -3,20 +3,26 @@ let popupOpened = document.querySelector(".popup_opened");
 let editButton = document.querySelector(".profile__edit-button");
 let closeIcon = document.querySelector(".popup__close-icon");
 let formElement = document.querySelector(".popup__form");
-let nameInput = document.querySelector(".popup__input-name");
-let aboutInput = document.querySelector(".popup__input-about");
-
+let nameInput = document.querySelector(".popup__input_type_name");
+let aboutInput = document.querySelector(".popup__input_type_about");
 let userName = document.querySelector(".profile__name");
 let userAbout = document.querySelector(".profile__about");
 
-editButton.addEventListener("click", function () {
+function openPopup() {
   popup.classList.add("popup_opened");
+  nameInput.value = userName.textContent;
+  aboutInput.value = userAbout.textContent;
+}
+
+function closePopup() {
+  popup.classList.remove("popup_opened");
+}
+editButton.addEventListener("click", function () {
+  openPopup();
 });
 
 closeIcon.addEventListener("click", function () {
-  popup.classList.remove(
-    "popup_opened"
-  ); /* подумывал о закрытии через нажатие по самому оверлею или по крестику. */
+  closePopup();
 });
 
 function formSubmitHandler(evt) {
@@ -25,21 +31,7 @@ function formSubmitHandler(evt) {
   let about = aboutInput.value;
   userName.textContent = name;
   userAbout.textContent = about;
-  popup.classList.remove("popup_opened");
+  closePopup();
 }
 
 formElement.addEventListener("submit", formSubmitHandler);
-
-/* нижеперечисленное - для рабочих лайков. */
-let elementLikes = document.querySelectorAll(".element__like");
-for (let elementLike of elementLikes) {
-  elementLike.addEventListener("click", function () {
-    let realSrc = elementLike.getAttribute("src");
-    if (realSrc !== "images/elements__card__like_active.svg") {
-      elementLike.setAttribute("src", "images/elements__card__like_active.svg");
-    } else {
-      elementLike.setAttribute("src", "images/elements__card__like.svg");
-    }
-  });
-}
-/*------------------------------------------*/
